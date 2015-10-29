8fab15091dfc319ded66a632cce122ae9744c949
Graham Murray <graham.murr@yahoo.ie>
2015-10-29 19:45:19 +0000

    Testing upload works

--- javascript/ajax-upload/ajax_upload_image_main.php
+++ javascript/ajax-upload/ajax_upload_image_main.php
@@ -1,24 +1,28 @@
 <html>
 <head>
 	<title>Ajax Image Upload Using PHP and jQuery</title>
-	<link rel="stylesheet" href="./css/upload-style.css" />
+	<link rel="stylesheet" href="../../css/image-upload-style.css" />
 	<link href='http://fonts.googleapis.com/css?family=Roboto+Condensed|Open+Sans+Condensed:300' rel='stylesheet' type='text/css'>
-	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-	<script src="upload-script.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+	<script src="image-upload-script.js"></script>
+
+	
 </head>
 <body>
 		<div class="main">
 			<h1>Upload Images for Post</h1><br/>
 			<hr>
-			<form id="uploadimage" action="" method="post" enctype="application/x-www-form-urlencoded">
-				<div id="image_preview"><img id="previewing" src="./images/noimage.png" /></div>
-					<hr id="line">
-				<div id="selectImage">
-					<label>Select Your Image</label><br/>
-					<input type="file" name="file" id="file" required />
-					<input type="submit" value="Upload" class="submit" />
-				</div>
-			</form>
+			<center>
+				<form id="uploadimage" action="" method="post" enctype="application/x-www-form-urlencoded">
+					<div id="image_preview"><img id="previewing" src="./images/noimage.png" /></div>
+						<hr id="line">
+					<div id="selectImage">
+						<label>Select Your Image</label><br/>
+						<input type="file" name="file" id="file" required />
+						<input type="submit" value="Upload" class="submit" />
+					</div>
+				</form>
+			</center>
 		</div>
 		<h4 id='loading' >loading..</h4>
 		<div id="message"></div>
