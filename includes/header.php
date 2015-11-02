8fa107912a90e67d700a1bd5ac4afd0b3fe19e53
Graham Murray <graham.murr@yahoo.ie>
2015-11-02 22:19:29 +0000

    Minor changes

--- includes/header.php
+++ includes/header.php
@@ -3,44 +3,50 @@
             <!-- Brand and toggle get grouped for better mobile display -->
             <div class="navbar-header">
                 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span>
+                    <span class="sr-only"></span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="#">FoodJackal</a>
-            </div>
+                <div>
+                	<img src="http://cloud.graham-murray.com/share/minecraft.png" height="40px" width="120px"/>
+                	<!--<a class="navbar-brand" href="#">Food Jackal</a>-->
+            	</div>
+        	</div>
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav">
                     <li>
-                        <a href="#">About</a>
+                        <a href="Contact">Vendors</a>
+                    </li>
+                    <li>
+                        <a href="Contact">Products</a>
                     </li>
                     <li>
                         <a href="">Services</a>
                     </li>
                     <li>
-                        <a href="Contact">Contact</a>
+                        <a href="#">About</a>
                     </li>
                 </ul>
 				<ul class="nav navbar-nav navbar-right">
 				<li>
 				 <form class="navbar-form" action="search.php?go" method="post" role="search">
-		<div class="input-group">
-			<input type="text" class="form-control" placeholder="Search" name="srchterm" id="srchterm">
-			<div class="input-group-btn">
-				<button class="btn btn-default" name ="submit" type="submit"><i class="glyphicon glyphicon-search"></i></button>
-			</div>
-		</div>
-		</form>
-		</li>
-					<li>
+					<div class="input-group">
+						<input type="text" class="form-control" placeholder="Search" name="srchterm" id="srchterm">
+						<div class="input-group-btn">
+							<button class="btn btn-default" name ="submit" type="submit"><i class="glyphicon glyphicon-search"></i></button>
+						</div>
+					</div>
+				</form>
+				</li>
+				<li>
 					<a href="Signup">Signup</a>
 				</li>
 				<li>
 					<a href="Login">Login</a>
 				</li>
-      </ul>
+      			</ul>
             </div>
             <!-- /.navbar-collapse -->
         </div>
